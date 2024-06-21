e452ad7ef4b9de554a77d236b811d5d9a035f8f6
Believemyself16 <han8b8@gmail.com>
2024-06-22 01:28:56 +0700

    final

--- Backend/TheaterWeb_Backend/obj/Debug/net6.0/TheaterWeb.AssemblyInfo.cs
+++ Backend/TheaterWeb_Backend/obj/Debug/net6.0/TheaterWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheaterWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85e30bcf3374ee58c36518d0efd7a42535bdfc14")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheaterWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheaterWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
